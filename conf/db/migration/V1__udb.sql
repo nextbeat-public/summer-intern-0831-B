74757ce5fcf6df9e868a7688e1be67da1394fe9f
池田敬祐 <keisuke0908@ike-keichan.local>
2019-09-01 14:21:40 +0900

    migrate file change 2nd

--- conf/db/migration/V1__udb.sql
+++ conf/db/migration/V1__udb.sql
@@ -11,7 +11,7 @@ CREATE TABLE "udb_user" (
   "created_at"                    TIMESTAMP    NOT     NULL DEFAULT CURRENT_TIMESTAMP,
   "user_detail"                   VARCHAR(255) DEFAULT NULL,
   "user_specialty_category"       VARCHAR(255) DEFAULT NULL,
-  "user_money"                    INT          DEFAULT 100000000 NOT NULL
+  "user_money"                    INT          DEFAULT 100000000 NOT NULL,
   UNIQUE KEY "udb_user_ukey01" ("email")
 ) ENGINE=InnoDB;
 
@@ -35,20 +35,16 @@ CREATE TABLE "udb_user_session" (
   UNIQUE KEY "udb_user_session_ukey01"("token")
 ) ENGINE=InnoDB;
 
-ALTER TABLE "udb_user" ADD CONSTRAINT PK_"udb_user" PRIMARY KEY ("id");
-
 CREATE TABLE "user_request" (
- "user_request_id"                INT          NOT     NULL AUTO_INCREMENT PRIMARY KEY,,
+ "user_request_id"                INT          NOT     NULL AUTO_INCREMENT PRIMARY KEY,
  "user_request_name"              VARCHAR(255) NOT     NULL,
  "user_request_detail"            VARCHAR(255) DEFAULT NULL,
  "user_request_date"              DATE,
- "user_request_cateqory"          VARCHAR(255) NOT     NULL,
+ "user_request_category"          VARCHAR(255) NOT     NULL,
  "location_id"                    INT,
  "user_request_good"              INT,
  "id"                             INT          NOT     NULL
 ) ENGINE=InnoDB;
-
-ALTER TABLE "user_request" ADD CONSTRAINT PK_"user_request" PRIMARY KEY ("user_request_id");
 
 CREATE TABLE "request_good_relationship" (
  "id"                             INT          NOT     NULL,
@@ -69,23 +65,10 @@ CREATE TABLE "teacher_request" (
  "id"                             INT          NOT     NULL
 ) ENGINE=InnoDB;
 
-ALTER TABLE "teacher_request" ADD CONSTRAINT PK_"teacher_request" PRIMARY KEY ("teacher_request_id");
-
 CREATE TABLE "participation_status_relationship" (
  "id"                             INT          NOT     NULL,
  "teacher_request_id"             INT          NOT     NULL
 ) ENGINE=InnoDB;
-
-ALTER TABLE "user_request" ADD CONSTRAINT FK_"user_request"_0 FOREIGN KEY ("id") REFERENCES "udb_user" ("id");
-
-ALTER TABLE "request_good_relationship" ADD CONSTRAINT FK_"request_good_relationship"_0 FOREIGN KEY ("id") REFERENCES "udb_user" ("id");
-ALTER TABLE "request_good_relationship" ADD CONSTRAINT FK_"request_good_relationship"_1 FOREIGN KEY ("user_request_id") REFERENCES "user_request" ("user_request_id");
-
-ALTER TABLE "teacher_request" ADD CONSTRAINT FK_"teacher_request"_0 FOREIGN KEY ("user_request_id") REFERENCES "user_reques"t ("user_request_id");
-ALTER TABLE "teacher_request" ADD CONSTRAINT FK_"teacher_request"_1 FOREIGN KEY ("id") REFERENCES "udb_user" ("id");
-
-ALTER TABLE "participation_status_relationship" ADD CONSTRAINT FK_"participation_status_relationship"_0 FOREIGN KEY ("id") REFERENCES "udb_user" ("id");
-ALTER TABLE "participation_status_relationship" ADD CONSTRAINT FK_"participation_status_relationship"_1 FOREIGN KEY ("teacher_request_id") REFERENCES "teacher_request" ("teacher_request_id");
 
 
 
