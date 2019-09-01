c188378b823a2ce09f2cf30902a18647e5b25cd0
closekn <close.me.kn@gmail.com>
2019-09-02 00:05:00 +0900

    insert miss

--- app/persistence/request/dao/RequestDao.scala
+++ app/persistence/request/dao/RequestDao.scala
@@ -73,7 +73,7 @@ class RequestDAO @javax.inject.Inject()(
   * requestの追加
   */
   def insert(request: Request) = {
-    val insertData: Organization = Organization(None, request.name, request.detail, LocalDateTime..now/*仮*/, request.numGood, request.categoryId.get, request.locationId)
+    val insertData: Request = Request(None, request.name, request.detail, LocalDateTime..now/*仮*/, request.numGood, request.categoryId.get, request.locationId)
     db.run {
       slick returning slick.map(_.id) += insertData
     }
