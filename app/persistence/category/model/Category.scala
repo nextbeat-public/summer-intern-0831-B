460930f815c9ea7ac7069d0fa31b74ec35ee6796
closekn <close.me.kn@gmail.com>
2019-09-01 17:36:49 +0900

    persistence/category の作成

---
+++ app/persistence/category/model/Category.scala
@@ -0,0 +1,21 @@
+package persistence.category.model
+
+import java.time.LocalDateTime
+import mvc.util.EnumStatus
+
+// 地域情報
+//~~~~~~~~~~
+case class Category(
+  id:           Category.Id,          // 全国地方公共団体コード
+  name:         String                // カテゴリ名
+  updatedAt:    LocalDateTime = LocalDateTime.now, // データ更新日
+  createdAt:    LocalDateTime = LocalDateTime.now  // データ作成日
+)
+
+// コンパニオン
+object Category {
+
+  // --[ 管理ID ]---------------------------------------------------------------
+  type Id = String
+
+}
